3e82c7aca50817f03314177c1de446231a66b4dd
fyqc <67112028+fyqc@users.noreply.github.com>
2020-09-29 00:52:06 -0400

    Update dgtle.py
    Multiple improvements:1. Since earlier this year, www.dgtle.com merged the web style from interest and article into the same format of url, and therefore the old way of extracting image location from webpage also needs to be changed.2. The server of Dgtle is very low quality, and the connection from North America to it seems very unstable. Shall there is a connection drop, I put a waiting period of 5 seconds.3. Sometimes, the poster uses an emoji from an external location, insert into the page as image, however, the url is completely different from the true image, and I have to remove those from the downloading list.4. For the same reason of 3, if the download url is separate apart from their location, then the download will have an error occurs. A new code added to remove those empty elements in the download list.5. Insead of changing the source code to change URL every time, an input function has been added, so users no longer need to modify the code, but only use the paste in the terminal to input the url.

--- dgtle.py
+++ dgtle.py
@@ -1,21 +1,20 @@
 import os 
-import requests 
+import time
+import requests
 from bs4 import BeautifulSoup 
 
+
 header = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:81.0) Gecko/20100101 Firefox/81.0'}
+URL = input('Please enter the webpage address from www.dgtle:')
 
-#The target webpage
-
-URL = 'https://www.dgtle.com/inst-1655874-1.html' 
-
-
+print('The web that open: ' + URL)
 
 if 'inst' in URL:
     
     print('This webpage is Inst type')
     # inst author
     html = requests.get(URL)
-    html.encoding = html.apparent_encoding
+    html.encoding = 'UTF-8'
     soup = BeautifulSoup(html.text, 'lxml')
     name = soup.select('div.own-img > div:nth-child(2) > span:nth-child(1)')
     title = soup.title.get_text()
@@ -37,7 +36,37 @@ elif 'article' in URL:
     print('This webpage is Article type')
     # article author
     html = requests.get(URL)
-    html.encoding = html.apparent_encoding
+    html.encoding = 'UTF-8'
+    soup = BeautifulSoup(html.text, 'lxml')
+    name = soup.select('.author')
+    title = soup.title.get_text()
+    for n2 in name:
+        dir_name = n2.get_text()
+        print('The Author is: ' + dir_name)
+        print('The webpage title is: ' + title)
+    
+    # article image
+    ori_url_b1 = soup.select('.articles-comment-left > figure > img')
+    ori_url_c1 = soup.find_all('div', class_='articles-comment-left')
+    downlist = []
+    for b1 in ori_url_b1:
+        ori_url_b2 = b1['src']
+        real_url_b = ori_url_b2.replace('_1800_500','')
+        downlist.append(real_url_b)
+    for c1 in ori_url_c1:
+        c2 = c1.find_all('img')
+        for n in range(0, len(c2)):
+            c3 = c2[n].get('src')
+            real_url_c = c3.replace('_1800_500','')
+            downlist.append(real_url_c)
+
+
+elif 'thread' in URL:
+
+    print('This webpage is Thread type')
+    # thread author
+    html = requests.get(URL)
+    html.encoding = 'UTF-8'
     soup = BeautifulSoup(html.text, 'lxml')
     name = soup.select('.author')
     title = soup.title.get_text()
@@ -46,31 +75,54 @@ elif 'article' in URL:
         print('The Author is ' + dir_name)
         print('The webpage title is: ' + title)
     
-    # article image
-    ori_url_b1 = soup.select('.articles-comment-left > figure > img')
+    # thread image
+    ori_url_c1 = soup.find_all('div', class_='articles-comment-left')
     downlist = []
-    for b1 in ori_url_b1:
-        ori_url_b2 = b1['src']
-        real_url_b = ori_url_b2.replace('_1800_500','')
-        downlist.append(real_url_b)
+    for c1 in ori_url_c1:
+        c2 = c1.find_all('img')
+        for n in range(0, len(c2)):
+            real_url_c = c2[n].get('src')
+            downlist.append(real_url_c)
 
 else:
+    # Just in case a new format is shown
     print('There is no key words in URL, please add new code to fix it.')
 
+#Remove the empty line in downlist
+while '' in downlist:
+    downlist.remove('')
+
+#Remove emoji from downlist
+for n in downlist:
+    if 'dgtle' not in n:
+        downlist.remove(n)
+
+# Create the folder with the author's id
 if not os.path.exists(dir_name):
     os.mkdir(dir_name)
 
-    
+# Download the file from the url list
 for url in downlist:
     file_name=url.split("/")[-1]
+
     with open(dir_name+'/'+ file_name, 'wb') as handle:
-            response = requests.get(url,headers=header, stream=True)
-
-            if not response.ok:
-                print(response)
-
-            for block in response.iter_content(1024):
-                if not block:
+            response = ''
+            while response == '':
+                try:
+                    response = requests.get(url, verify=False, headers=header, stream=True)
+                    if not response.ok:
+                        print(response)
+                    for block in response.iter_content(1024):
+                        if not block:
+                            break
+                        handle.write(block)
                     break
+                except:
+                    print("Connection dropped..")
+                    print("Wait for 5 seconds")
+                    time.sleep(5)
+                    print("Continue...")
+                    continue
 
-                handle.write(block)
+print('Done, Wait for 2 seconds to start the next one.')
+time.sleep(2)
