9f0a787fb5d6da384af1654aa63b9ddd9c4f73ca
fyqc <67112028+fyqc@users.noreply.github.com>
2024-10-29 02:18:03 -0400

    Adding logging module

--- dgtle.py
+++ dgtle.py
@@ -3,6 +3,7 @@ import time
 import requests
 from bs4 import BeautifulSoup
 from threading import Thread
+import logging
 
 """
 Create an empty folder, that contains all the webpage url shortcuts from Dgtle.com
@@ -12,7 +13,14 @@ images will be downloaded as their original resolution and
 stored into each folder by author's id.
 """
 
-# 4/4/2024
+logging.basicConfig(
+    format='%(asctime)s %(levelname)s %(message)s %(lineno)s',
+    level=logging.INFO,
+    handlers=[logging.FileHandler(filename='dgtle.log',mode='a',encoding='utf-8')]
+)
+
+
+# 9/1/2024
 # 12:49 PM
 
 
@@ -23,10 +31,10 @@ def internet_shortcut(rootdir=os.getcwd()):
     webpage_list = []
     for (_, _, filenames) in os.walk(rootdir):
         for filename in filenames:
-            if filename.endswith('.URL'):
+            if filename.lower().endswith('.url'):
                 with open(os.path.join(rootdir, filename), "r", encoding='utf-8') as f:
                     webpage = f.read().split('\n')[1][4:]
-                    # Just be sure the data we acquired is URL.
+                    # Just be sure the data we acquired is URL
                     if webpage.startswith('http'):
                         webpage_list.append(webpage)
     return webpage_list
@@ -37,14 +45,13 @@ def get_soup_from_webpage(url, header):
     USE BEAUTIFULSOUP TO PARSING HTML AND XML DOCUMENTS.
     '''
     response = requests.get(url, headers=header)
+    # The encoding here shall be modified as per the web source.
     response.encoding = 'utf-8'
     return BeautifulSoup(response.text, 'lxml')
 
 
+# FIND TITLE USING BS4
 def find_title(soup):
-    '''
-    FIND TITLE USING BS4.
-    '''
     return soup.title.get_text()
 
 
@@ -52,12 +59,14 @@ def find_author(soup):
     '''
     FIND AUTHOR AND USE IT AS FOLDER'S NAME.
     '''
-    # This is for Inst page, and if it is not, which will return None.
+    # This is for Inst page, and if it is not, which will return None
     if soup.find('div', class_='interset-content-top'):
-        # In that case, skip this line, goto next condition.
-        return soup.find('div', class_='interset-content-top').get_text().strip().split("\n")[0]
+        # In that case, skip this line, goto next condition
+        author = soup.find(
+            'div', class_='interset-content-top').get_text().strip().split("\n")[0]
     else:  # Choose Article page instead.
-        return soup.find('span', class_='author').get_text()
+        author = soup.find('span', class_='author').get_text()
+    return author
 
 
 def extract_image_url(soup):
@@ -76,24 +85,34 @@ def extract_image_url(soup):
 
     tags = soup.find_all('img')
     for n in tags:
-        # Article Type.
+        # Article Type
         if n.get('data-original'):
             raw_img_url = n.get('data-original')
-        # Inst Type and majority of Article Type.
+        # Inst Type and majority of Article Type
         elif n['src']:
             raw_img_url = n.get('src')
-        # Here we trim the URL, for origin resolution.
-        # On Jul 15, 2021, There is a new case found, the URL has no format attached,
-        # as shown below, the real file which is jpg in fact.
-        # http://s1.dgtle.com/dgtle_img/article/2021/07/15/74e7f202107151213512077_1800_500_w
-        img_url = raw_img_url.replace('_1800_500_w.', '.').split("?")[0]
-        img_url = img_url.replace('_1800_500.', '.').split("?")[0]
 
-        if 'dgtle_img/article' in img_url or 'dgtle_img/ins' in img_url:
+        # On May 25, 2024, There is a new case found
+        if 'p3-sign.toutiaoimg.com' in raw_img_url:
+            img_url = raw_img_url
+
+        else:
+            # Here we trim the URL, for origin resolution.
+            # On Jul 15, 2021, There is a new case found, the URL has no format attached,
+            # as shown below, the real file which is jpg in fact.
+            # http://s1.dgtle.com/dgtle_img/article/2021/07/15/74e7f202107151213512077_1800_500_w
+            img_url = raw_img_url.replace('_1800_500_w.', '.').split("?")[0]
+            img_url = img_url.replace('_1800_500.', '.').split("?")[0]
+
+        keywords = ['dgtle_img/article',
+                    'dgtle_img/ins', 'p3-sign.toutiaoimg.com']
+        if any(keyword in img_url for keyword in keywords):
+            # if 'dgtle_img/article' in img_url or 'dgtle_img/ins' in img_url or 'p3-sign.toutiaoimg.com' in img_url:
             downlist.append(img_url)
 
     if len(downlist) == 0:
         print("Nothing found here, website might been updated, it's time for us to update code.")
+        logging.warning("Nothing found here, website might been updated, it's time for us to update code.")
 
     # Remove duplicated images, just incase.
     downlist = list(set(downlist))
@@ -139,39 +158,65 @@ def make_folder_asper_author(dir_name):
 
 
 def rillaget(link, dir_name, header):
-    '''
-    THE CORE OF THIS CODE IS DOWNLOAD IMAGE W/O ANY ISSUES.
-    '''
-    filename = link.split("/")[-1]
-    
-    # On January 20, 2024, it was discovered that some pictures did not have suffixes, but were actually in jpeg format.
-    # http://s1.dgtle.com/dgtle_img/article/2024/01/01/2e6b7202401012057028574_1800_500_w.Zuiko PRO」开向山的大门
-    # On April 04, 2024, it was discovered that some pictures did not have suffixes, but were actually in jpeg format.
-    # d7e06202403251246431344_1800_500_w.313
-    
-    extentions = ['.jpg', '.png', '.jpeg', '.gif']
-    if not any(ext in filename for ext in extentions):
-        filename = "".join([filename, '.jpeg'])
-        
-    total_path = os.path.join(dir_name, filename)
     attempts = 0
     success = False
-    while attempts < 5 and not success:
+    while attempts < 8 and not success:
         try:
             response = requests.get(link, headers=header, timeout=5)
-            
-            if len(response.content) == int(response.headers['Content-Length']):
-                with open(total_path, 'wb') as fd:
-                    for chunk in response.iter_content(1024):
-                        fd.write(chunk)
-                print(filename + "  Successfully downloaded")
-                success = True
-        except:
-            print(f'{filename} has connection error, retry after 5 seconds for the {attempts + 1} attempts')
-            time.sleep(5)
+
+            if response.status_code == 200:
+                if len(response.content) == int(response.headers['Content-Length']):
+                    filename = link.split("/")[-1]
+                    if ".image" in filename:  # toutiaoimg.com
+                        filename = filename.split(".")[0]
+
+                    extentions = ['.jpg', '.png', '.jpeg', '.gif', '.webp']
+                    if not any(ext in filename for ext in extentions):
+                        content_type_name = response.headers.get(
+                            'Content-Type')
+                        image_format = content_type_name.split('/')[-1]
+                        filename = ".".join([filename, image_format])
+
+                    total_path = os.path.join(dir_name, filename)
+
+                    expected_length = int(
+                        response.headers.get('Content-Length', 0))
+                    current_length = 0
+
+                    with open(total_path, 'wb') as f:
+                        for chunk in response.iter_content(1024):
+                            f.write(chunk)
+                            current_length += len(chunk)
+
+                            # Content-Length check
+                            if current_length >= expected_length:
+                                break
+                    print(filename + "  Successfully downloaded")
+                    logging.info(filename + "  Successfully downloaded")
+                    success = True
+
+            elif response.status_code == 403:
+                print(f'{link} has connection error, retry one more time.)
+                logging.error(f'{link} has connection error, retry one more time.)
+                time.sleep(5)
+                attempts += 6
+
+            else:
+                print(f'{link} has connection error, retry after 5 seconds for the {attempts + 1} attempts')
+                logging.error(f'{link} has connection error, retry after 5 seconds for the {attempts + 1} attempts')
+                time.sleep(5*attempts + 5)
+                attempts += 1
+
+        except Exception as e:
+            print(
+                f'{link} has connection error, retry after 5 seconds for the {attempts + 1} attempts')
+            logging.error(f'{link} has connection error, retry after 5 seconds for the {attempts + 1} attempts')
+            time.sleep(5*attempts + 5)
             attempts += 1
-    if attempts == 5:
-        print(f"{filename} Failed to downloaded.")
+
+    if attempts == 8:
+        print(f"{filename}  Failed to downloaded. \n{e}")
+        logging.error(f"{filename}  Failed to downloaded. \n{e}")
 
 
 if __name__ == '__main__':
@@ -182,6 +227,7 @@ if __name__ == '__main__':
     for weblink in webpage_list:
         dir_name, downlist = try_soup_ten_times(weblink, header)
         if downlist == "404":
+            logging.critical("downlist is not existing.")
             continue
         make_folder_asper_author(dir_name)
         threads = []
@@ -191,3 +237,5 @@ if __name__ == '__main__':
             threads.append(t)
         for t in threads:
             t.join()
+
+        print()
